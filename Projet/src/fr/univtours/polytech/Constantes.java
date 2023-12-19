1b6efecf5c00f3ec6816127e0d1e27c043bdaaa2
Sheerven <ayordletamer@gmail.com>
2023-12-19 12:58:26 +0100

    Ajout trucs dans constantes + avancement sur prio urgence absolue

--- Projet/src/fr/univtours/polytech/Constantes.java
+++ Projet/src/fr/univtours/polytech/Constantes.java
@@ -2,13 +2,21 @@ package fr.univtours.polytech;
 
 import java.time.LocalTime;
 
+import fr.univtours.polytech.ressource.Salle;
+
 public class Constantes {
 	private LocalTime tempsPreparation;
 	private LocalTime tempsAnesthesie;
 	private LocalTime tempsLiberation;
+	private LocalTime tempsMoyen;
 	private Integer nbReserve;
 	private Integer tempsMoyenOperation;
-	private LocalTime tempsMoyen;
+
+	private LocalTime tempsMoyenDepuisAttentePreparation;
+	private LocalTime tempsMoyenDepuisAttenteOperation;
+	private LocalTime tempsMoyenDepuisAttenteLiberation;
+
+	private Double marge;
 
 	public LocalTime getTempsPreparation() {
 		return tempsPreparation;
@@ -42,21 +50,90 @@ public class Constantes {
 		this.nbReserve = nbReserve;
 	}
 
-	public Constantes(LocalTime tempsPreparation, LocalTime tempsAnesthesie, LocalTime tempsLiberation, Integer nbReserve, Integer tempsMoyenOperation) {
+	public Constantes(LocalTime tempsPreparation, LocalTime tempsAnesthesie, LocalTime tempsLiberation,
+			Integer nbReserve, Integer tempsMoyenOperation) {
 		this.tempsPreparation = tempsPreparation;
 		this.tempsAnesthesie = tempsAnesthesie;
 		this.tempsLiberation = tempsLiberation;
 		this.nbReserve = nbReserve;
 		this.tempsMoyenOperation = tempsMoyenOperation;
-		this.setTempsMoyen(tempsPreparation.plusHours(tempsAnesthesie.getHour() + tempsLiberation.getHour()).plusMinutes(tempsAnesthesie.getMinute() + tempsLiberation.getMinute() + tempsMoyenOperation));
+
+		this.marge = 0.1;
+		setTempsMoyen();
+
+		tempsMoyenDepuisAttentePreparation = getHeureMarge(tempsPreparation);
+		tempsMoyenDepuisAttentePreparation = tempsMoyen.minusHours(tempsMoyenDepuisAttentePreparation.getHour())
+				.minusMinutes(tempsMoyenDepuisAttentePreparation.getMinute());
+		tempsMoyenDepuisAttenteOperation = getHeureMarge(0, tempsMoyenOperation);
+		tempsMoyenDepuisAttenteOperation = tempsMoyenDepuisAttentePreparation.minusHours(tempsMoyenDepuisAttenteOperation.getHour())
+				.minusMinutes(tempsMoyenDepuisAttenteOperation.getMinute());
+		tempsMoyenDepuisAttenteLiberation = getHeureMarge(tempsLiberation);
+		tempsMoyenDepuisAttenteLiberation = tempsMoyenDepuisAttenteOperation.minusHours(tempsMoyenDepuisAttenteLiberation.getHour())
+				.minusMinutes(tempsMoyenDepuisAttenteLiberation.getMinute());
 	}
 
 	public LocalTime getTempsMoyen() {
 		return tempsMoyen;
+	}
+	
+	public LocalTime getTempsMoyen(Salle.listeEtats etat) {
+		switch(etat) {
+		case ATTENTEPREPARATION:
+		case PREPARATION:
+			return tempsMoyenDepuisAttentePreparation;
+		case ATTE:
+		case PREPARATION:
+			return 
+		case ATTENTEPREPARATION:
+		case PREPARATION:
+			return 
+		case ATTENTEPREPARATION:
+		case PREPARATION:
+			return 
+		default :
+			return tempsMoyen;
+		}
 	}
 
 	public void setTempsMoyen(LocalTime tempsMoyen) {
 		this.tempsMoyen = tempsMoyen;
 	}
 
+	public void setTempsMoyen() {
+		int nombreHeure = tempsPreparation.getHour() + tempsAnesthesie.getHour() + tempsLiberation.getHour();
+		int nombreMinute = tempsPreparation.getMinute() + tempsAnesthesie.getMinute() + tempsMoyenOperation
+				+ tempsLiberation.getMinute();
+
+		tempsMoyen = getHeureMarge(nombreHeure, nombreMinute);
+	}
+
+	private LocalTime getHeureMarge(LocalTime heure, Integer nombreHeure, Integer nombreMinute) {
+		if (heure != null) {
+			nombreHeure = heure.getHour();
+			nombreMinute = heure.getMinute();
+		}
+		double nombreHeureMarge = nombreHeure * marge;
+		int nombreHeureMargeEntier = (int) nombreHeureMarge;
+		nombreHeure += nombreHeureMargeEntier;
+
+		int nombreMinuteMargeDepuisHeure = (int) (nombreHeureMarge - nombreHeureMarge) * 60;
+		double nombreMinuteMarge = nombreMinute * marge;
+		int nombreMinuteMargeEntier = nombreMinuteMargeDepuisHeure + ((int) nombreMinuteMarge);
+		nombreMinute += nombreMinuteMargeEntier;
+
+		int nbHeureDsMinute = nombreMinuteMargeEntier / 60;
+
+		nombreHeure += nbHeureDsMinute;
+		nombreMinute -= nbHeureDsMinute * 60;
+
+		return LocalTime.of(nombreHeure, nombreMinute);
+	}
+
+	public LocalTime getHeureMarge(LocalTime heure) {
+		return getHeureMarge(heure, 0, 0);
+	}
+
+	public LocalTime getHeureMarge(Integer nombreHeure, Integer nombreMinute) {
+		return getHeureMarge(null, nombreHeure, nombreMinute);
+	}
 }
--- Projet/src/fr/univtours/polytech/reglesgestions/GPPrioriteAbsoluUrgence.java
+++ Projet/src/fr/univtours/polytech/reglesgestions/GPPrioriteAbsoluUrgence.java
@@ -1,8 +1,18 @@
 package fr.univtours.polytech.reglesgestions;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import fr.univtours.polytech.Constantes;
 import fr.univtours.polytech.Planning;
 import fr.univtours.polytech.Simulation;
 import fr.univtours.polytech.entite.Patient;
+import fr.univtours.polytech.entite.PatientRDV;
+import fr.univtours.polytech.ressource.Salle;
+import fr.univtours.polytech.util.TrouPlanning;
 
 /**
  * Regle de gestion qui lorsque qu'un patient urgent est declare dans la
@@ -17,7 +27,126 @@ public class GPPrioriteAbsoluUrgence implements GestionPlanning {
 	}
 
 	public Planning solution(Patient patientUrgent) {
+		// Initialisation de la nouvelle liste de patients
+		List<Patient> nouvListePatientRDV = new ArrayList<>();
+		List<Patient> nouvListePatientUrgent = new ArrayList<>();
+		List<Patient> listePatient;
 
-		return null;
+		if (patientUrgent != null) {
+			// Si un patient urgent est donné, récupérer le planning existant et l'ajouter à
+			// la nouvelle liste
+			Planning ancienPlanning = simulation.getPlanning();
+
+			listePatient = ancienPlanning.extraiteDonnee();
+
+			nouvListePatientUrgent.add(patientUrgent);
+			System.out.println("changememnt planning");
+		} else {
+			return new GPPrioritePremierArriveReserveStatique(simulation).solution(null); // On applique la règle de Gestion la plus simple
+		}
+
+		for (Patient p : listePatient) {
+			if (p.estUrgent()) {
+				nouvListePatientUrgent.add(p);
+			} else {
+				nouvListePatientRDV.add(p);
+			}
+		}
+
+		// Tri de la liste de patient en fcontion du temps d'arrivée
+		Collections.sort(nouvListePatientRDV, (p1, p2) -> p1.getHeureArrive().compareTo(p2.getHeureArrive()));
+		Collections.sort(nouvListePatientUrgent, (p1, p2) -> p1.getHeureArrive().compareTo(p2.getHeureArrive()));
+
+		// Récupération des salles de la simulation
+		Map<Salle.typeSalles, List<Salle>> sallesMap = simulation.getSalles();
+		List<Salle> pileSalleUrgent = new ArrayList<Salle>(); // Pile utilisée pour placer les patients Urgents
+		List<Salle> pileSalleRDV = new ArrayList<Salle>(); // Pile utilisée pour placer les patients RDV
+		
+		List<Salle> sallesTresEquipees = new ArrayList<Salle>(); // Servira à chercher les trous possibles entre les patientsUrgents pour placer les patientsRDV
+		Map<Salle, List<TrouPlanning>> mapTrousParSalle; // Map qui stokera les trous trouvés
+		
+		Map<Salle, List<Patient>> renvoi = new HashMap<Salle, List<Patient>>(); // Le Map qui sera stocker dans le planning
+		
+		Constantes constantes = simulation.getConstantes();
+		
+		// Tri des salles
+		Salle.typeSalles type;
+		for (Salle.listeEtats etat : Salle.listeEtats.values()) {
+			type = Salle.typeSalles.RESERVE;
+			if (sallesMap.containsKey(type)) {
+				for (Salle salle : sallesMap.get(type)) {
+					if (salle.getEtat() == etat) {
+						pileSalleUrgent.add(salle);
+						renvoi.put(salle, new ArrayList<Patient>());
+					}
+				}
+			}
+			
+			type = Salle.typeSalles.PEUEQUIPE;
+			if (sallesMap.containsKey(type)) {
+				for (Salle salle : sallesMap.get(type)) {
+					if (salle.getEtat() == etat) {
+						pileSalleRDV.add(salle);
+						renvoi.put(salle, new ArrayList<Patient>());
+						
+						List<TrouPlanning> listeTrous = new ArrayList<>();
+						listeTrous.add(new TrouPlanning(constantes.get));
+						mapTrousParSalle.put(salle, listeTrous);
+					}
+				}
+			}
+			
+			type = Salle.typeSalles.RESERVE;
+			if (sallesMap.containsKey(type)) {
+				for (Salle salle : sallesMap.get(type)) {
+					if (salle.getEtat() == etat) {
+						pileSalleUrgent.add(salle);
+						renvoi.put(salle, new ArrayList<Patient>());
+					}
+				}
+			}
+			
+			type = Salle.typeSalles.TRESEQUIPE;
+			if (sallesMap.containsKey(type)) {
+				for (Salle salle : sallesMap.get(type)) {
+					if (salle.getEtat() == etat) {
+						pileSalleUrgent.add(salle);
+						pileSalleRDV.add(salle);
+						sallesTresEquipees.add(salle);
+						
+						renvoi.put(salle, new ArrayList<Patient>());
+					}
+				}
+			}
+			
+		}
+
+		for (Patient patient : nouvListePatientRDV) {
+//			System.out.println("On place le patient " + patient.getId() + "   - Gravite / urgent : "
+//					+ patient.getGravite() + " / " + patient.estUrgent());
+
+			int indice = 0;
+
+			boolean place = false;
+			Salle salle;
+			while (!place) {
+				salle = pileSalle.get(indice);
+				// System.out.println("Salle " + salle.getId() + " - Gravite : " +
+				// salle.getType());
+
+				if (salle.getType() == Salle.typeSalles.TRESEQUIPE || salle.getType() == Salle.typeSalles.RESERVE) {
+					renvoi.get(salle).add(patient);
+					place = true;
+
+					pileSalle.remove(indice);
+					pileSalle.add(salle);
+				}
+
+			}
+
+			indice++;
+		}
 	}
-}
+
+	return new Planning(renvoi);
+}}
